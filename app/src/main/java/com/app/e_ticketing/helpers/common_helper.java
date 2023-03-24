fd5653ecb6694c4020b6b1bc08be4b32724e6947
vineetharavuri2 <vineetharavuri2@gmail.com>
2023-03-23 23:46:54 -0500

    I am creating a commonhelper on it

--- app/src/main/java/com/app/e_ticketing/helpers/common_helper.java
+++ app/src/main/java/com/app/e_ticketing/helpers/common_helper.java
@@ -9,14 +9,16 @@ import java.util.List;
 
 public class common_helper {
     public static String collection_users = "users";
-    public static boolean getOfficerLogin(String email, String password){
+
+    public static boolean getOfficerLogin(String email, String password) {
         return email.equals("officer@gmail.com") && password.equals("officer123");
     }
-    public static boolean getUserLogin(String email, String password){
+
+    public static boolean getUserLogin(String email, String password) {
         return email.equals("user@gmail.com") && password.equals("user123");
     }
 
-    public static List<User> getUsersList(){
+    public static List<User> getUsersList() {
         List<User> list = new ArrayList<>();
         list.add(new User("1",
                 "Lucas@gmail.com",
@@ -48,7 +50,7 @@ public class common_helper {
         return list;
     }
 
-    public static List<Ticket> getTicketList(){
+    public static List<Ticket> getTicketList() {
         List<Ticket> list = new ArrayList<>();
         list.add(new Ticket("1",
                 "Crossed Speed limit & Dangerous Driving",
@@ -80,11 +82,10 @@ public class common_helper {
         List<Ticket> list = getTicketList();
         Ticket ticket = new Ticket();
         for (int i = 0; i < list.size(); i++) {
-            if(list.get(i).getId().equals(id))
+            if (list.get(i).getId().equals(id))
                 ticket = list.get(i);
         }
         return ticket;
     }
-
-
 }
+
